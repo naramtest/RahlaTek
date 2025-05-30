ba0baab35503715d3d67d344f82578d7f6192521
naram <naramalkoht@gmail.com>
2025-05-30 10:26:00 +0300

    add relation between tenants and users

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Stancl\Tenancy\Contracts\TenantWithDatabase;
 use Stancl\Tenancy\Database\Concerns\HasDatabase;
 use Stancl\Tenancy\Database\Concerns\HasDomains;
@@ -10,4 +11,17 @@ use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
 class Tenant extends BaseTenant implements TenantWithDatabase
 {
     use HasDatabase, HasDomains;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public static function getCustomColumns(): array
+    {
+        return [
+            'id',
+            'user_id',
+        ];
+    }
 }
