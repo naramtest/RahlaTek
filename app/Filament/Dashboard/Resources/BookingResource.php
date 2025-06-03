7a799658eebd88783a14163684e362831380e489
naram <naramalkoht@gmail.com>
2025-06-03 10:55:14 +0300

    move migration into tenant folder

---
+++ app/Filament/Dashboard/Resources/BookingResource.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Filament\Dashboard\Resources;
+
+use App\Filament\Dashboard\Resources\BookingResource\Pages;
+use App\Models\Booking;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class BookingResource extends Resource
+{
+    protected static ?string $model = Booking::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBookings::route('/'),
+            'create' => Pages\CreateBooking::route('/create'),
+            'edit' => Pages\EditBooking::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Dashboard/Resources/BookingResource/Pages/CreateBooking.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Dashboard\Resources\BookingResource\Pages;
+
+use App\Filament\Dashboard\Resources\BookingResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateBooking extends CreateRecord
+{
+    protected static string $resource = BookingResource::class;
+}
